60ec5b6347024e19906bc6de98d26bf38ac43178
Benson Maruchu <benmaruchu@gmail.com>
2019-08-14 11:18:31 +0300

    feat: add performance report

--- src/http.router.js
+++ src/http.router.js
@@ -16,11 +16,12 @@ import _ from 'lodash';
 import { Router } from '@lykmapipo/express-common';
 import { getString } from '@lykmapipo/env';
 import getOverviewReport from './reports/overview';
+import getPerformanceReport from './reports/performance';
 
 /* local constants */
 const API_VERSION = getString('API_VERSION', '1.0.0');
 const PATH_OVERVIEW = '/reports/overview';
-// const PATH_PERFORMANCE = '/reports/performance';
+const PATH_PERFORMANCE = '/reports/performance';
 
 const router = new Router({
   version: API_VERSION,
@@ -41,10 +42,19 @@ router.get(PATH_OVERVIEW, function getOverview(request, response, next) {
   });
 });
 
-// router.get(PATH_PERFORMANCE, function getPerformance(
-//   request,
-//   response,
-//   next
-// ) {});
+router.get(PATH_PERFORMANCE, function getPerformance(request, response, next) {
+  const options = _.merge({}, request.mquery);
+
+  const filter = options.filter || {};
+
+  getPerformanceReport(filter, (error, results) => {
+    if (error) {
+      next(error);
+    } else {
+      response.status(200);
+      response.json(results);
+    }
+  });
+});
 
 export default router;
