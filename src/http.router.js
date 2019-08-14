d2d8ec42ee5c3ccbf36870793adc53648e5e6b57
Benson Maruchu <benmaruchu@gmail.com>
2019-08-14 12:11:45 +0300

    feat: add initial operator report

--- src/http.router.js
+++ src/http.router.js
@@ -17,17 +17,19 @@ import { Router } from '@lykmapipo/express-common';
 import { getString } from '@lykmapipo/env';
 import getOverviewReport from './reports/overview';
 import getPerformanceReport from './reports/performance';
+import getOperatorPerformanceReport from './reports/operator';
 
 /* local constants */
 const API_VERSION = getString('API_VERSION', '1.0.0');
 const PATH_OVERVIEW = '/reports/overview';
 const PATH_PERFORMANCE = '/reports/performance';
+const PATH_OPERATOR_PERFORMANCE = '/reports/operator/:id';
 
 const router = new Router({
   version: API_VERSION,
 });
 
-router.get(PATH_OVERVIEW, function getOverview(request, response, next) {
+router.get(PATH_OVERVIEW, (request, response, next) => {
   const options = _.merge({}, request.mquery);
 
   const filter = options.filter || {};
@@ -42,12 +44,31 @@ router.get(PATH_OVERVIEW, function getOverview(request, response, next) {
   });
 });
 
-router.get(PATH_PERFORMANCE, function getPerformance(request, response, next) {
+router.get(PATH_PERFORMANCE, (request, response, next) => {
   const options = _.merge({}, request.mquery);
 
   const filter = options.filter || {};
 
   getPerformanceReport(filter, (error, results) => {
+    if (error) {
+      next(error);
+    } else {
+      response.status(200);
+      response.json(results);
+    }
+  });
+});
+
+router.get(PATH_OPERATOR_PERFORMANCE, (request, response, next) => {
+  const options = _.merge({}, request.mquery);
+
+  let filter = options.filter || {};
+
+  if (request.params.id) {
+    filter = _.merge({}, filter, { operator: request.params.id });
+  }
+
+  getOperatorPerformanceReport(filter, (error, results) => {
     if (error) {
       next(error);
     } else {
