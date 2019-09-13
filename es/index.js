ea7374bb3b819e8f67b477d957d147e5aa1509e7
Benson Maruchu <benmaruchu@gmail.com>
2019-09-13 11:10:34 +0300

    chore(build): bundle library for testing

--- es/index.js
+++ es/index.js
@@ -811,27 +811,6 @@ const normalizeTime = time => {
 
 /**
  * @function
- * @name normalizeObjectTimes
- * @description Normalize times in a provided object (item)
- *
- * @param {object} item Object with times to be normalized
- * @returns {object} Object with averageResolve time and averageAttend Time parse
- *
- * @version 0.1.0
- * @since 0.2.0
- */
-const normalizeObjectTimes = item => {
-  const normalizeObject = {};
-
-  normalizeObject.averageResolveTime = normalizeTime(item.averageResolveTime);
-
-  normalizeObject.averageAttendTime = normalizeTime(item.averageAttendTime);
-
-  return { ...item, ...normalizeObject };
-};
-
-/**
- * @function
  * @name normalizeMetricTimes
  * @description Normalize aggregation object with metric times to a standard
  * format. Also parse those times to human readable format
@@ -939,12 +918,12 @@ const prepareReportResponse = results => {
   }
 
   if (data.types) {
-    data.types = map(data.types, normalizeObjectTimes);
+    data.types = map(data.types, normalizeMetricTimes);
   }
 
-  if (data.methods) {
-    data.methods = map(data.methods, normalizeObjectTimes);
-  }
+  // if (data.methods) {
+  //   data.methods = map(data.methods, normalizeObjectTimes);
+  // }
 
   return { ...defaultResults, data };
 };
