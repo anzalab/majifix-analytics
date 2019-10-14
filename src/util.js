2d9499f6e299f8fce91da5c808cc20dae4cf8e55
Benson Maruchu <benmaruchu@gmail.com>
2019-10-14 10:57:41 +0300

    refactor: handle more cases in prepareReportResponse

--- src/util.js
+++ src/util.js
@@ -123,7 +123,7 @@ export const prepareReportResponse = results => {
 
   const data = merge(...results);
 
-  data.overall = head(data.overall);
+  data.overall = head(data.overall) || {};
 
   if (data.overall) {
     data.overall = normalizeMetricTimes(data.overall);
